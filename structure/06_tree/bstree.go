96aa43add1663b4fcd3127a2b56120075450d801
abean <abeanz@163.com>
2025-01-08 15:59:55 +0800

    feat: structure tree

--- structure/06_tree/bstree.go
+++ structure/06_tree/bstree.go
@@ -1,22 +1,15 @@
-// Binary search tree.
-//
-// For more details check out those links below here:
-// Wikipedia article: https://en.wikipedia.org/wiki/Binary_search_tree
-// see bstree.go
-
 package tree
 
 import "learn-go/structure/constraints"
 
-// Verify Interface Compliance
 var _ Node[int] = &BSNode[int]{}
 
-// BSNode represents a single node in the BinarySearch.
+// BSNode 是二叉搜索树的节点结构
 type BSNode[T constraints.Ordered] struct {
-	key    T
-	parent *BSNode[T]
-	left   *BSNode[T]
-	right  *BSNode[T]
+	key    T          // 节点存储的值
+	parent *BSNode[T] // 父节点指针
+	left   *BSNode[T] // 左子节点指针
+	right  *BSNode[T] // 右子节点指针
 }
 
 func (n *BSNode[T]) Key() T {
@@ -35,14 +28,13 @@ func (n *BSNode[T]) Right() Node[T] {
 	return n.right
 }
 
-// BinarySearch represents a Binary-Search tree.
-// By default, _NIL = nil.
+// BinarySearch 是二叉搜索树的主结构
 type BinarySearch[T constraints.Ordered] struct {
-	Root *BSNode[T]
-	_NIL *BSNode[T] // a sentinel value for nil
+	Root *BSNode[T] // 树的根节点
+	_NIL *BSNode[T] // 哨兵节点，用于表示空节点
 }
 
-// NewBinarySearch creates a novel Binary-Search tree
+// NewBinarySearch 创建一个新的二叉搜索树
 func NewBinarySearch[T constraints.Ordered]() *BinarySearch[T] {
 	return &BinarySearch[T]{
 		Root: nil,
@@ -50,19 +42,18 @@ func NewBinarySearch[T constraints.Ordered]() *BinarySearch[T] {
 	}
 }
 
-// Empty determines the Binary-Search tree is empty
 func (t *BinarySearch[T]) Empty() bool {
 	return t.Root == t._NIL
 }
 
-// Push a chain of Node's into the BinarySearch
+// Push 向树中插入一个或多个值
 func (t *BinarySearch[T]) Push(keys ...T) {
 	for _, key := range keys {
 		t.pushHelper(t.Root, key)
 	}
 }
 
-// Delete removes the node of val
+// Delete 从树中删除一个值
 func (t *BinarySearch[T]) Delete(val T) bool {
 	node, ok := t.Get(val)
 	if !ok {
@@ -72,60 +63,45 @@ func (t *BinarySearch[T]) Delete(val T) bool {
 	return true
 }
 
-// Get a Node from the Binary-Search Tree
 func (t *BinarySearch[T]) Get(key T) (Node[T], bool) {
 	return searchTreeHelper[T](t.Root, t._NIL, key)
 }
 
-// Has Determines the tree has the node of Key
 func (t *BinarySearch[T]) Has(key T) bool {
 	_, ok := searchTreeHelper[T](t.Root, t._NIL, key)
 	return ok
 }
 
-// PreOrder Traverses the tree in the following order Root --> Left --> Right
 func (t *BinarySearch[T]) PreOrder() []T {
 	traversal := make([]T, 0)
 	preOrderRecursive[T](t.Root, t._NIL, &traversal)
 	return traversal
 }
 
-// InOrder Traverses the tree in the following order Left --> Root --> Right
 func (t *BinarySearch[T]) InOrder() []T {
 	return inOrderHelper[T](t.Root, t._NIL)
 }
 
-// PostOrder traverses the tree in the following order Left --> Right --> Root
 func (t *BinarySearch[T]) PostOrder() []T {
 	traversal := make([]T, 0)
 	postOrderRecursive[T](t.Root, t._NIL, &traversal)
 	return traversal
 }
 
-// LevelOrder returns the level order traversal of the tree
-// LevelOrder 返回二叉搜索树的层序遍历结果。
-// 它返回一个切片，其中包含树中节点的值，按照层级顺序排列。
-// 该方法使用辅助函数 levelOrderHelper 进行递归遍历。
 func (t *BinarySearch[T]) LevelOrder() []T {
 	traversal := make([]T, 0)
 	levelOrderHelper[T](t.Root, t._NIL, &traversal)
 	return traversal
 }
 
-// AccessNodesByLayer accesses nodes layer by layer (2-D array),  instead of printing the results as 1-D array.
-// AccessNodesByLayer 返回一个二维切片，包含按层访问的二叉搜索树节点。
-// 每个子切片代表树的一层，节点按从左到右的顺序排列。
-// 该方法使用辅助函数 accessNodeByLayerHelper 来实现层次遍历。
 func (t *BinarySearch[T]) AccessNodesByLayer() [][]T {
 	return accessNodeByLayerHelper[T](t.Root, t._NIL)
 }
 
-// Depth returns the calculated depth of a binary search tree
 func (t *BinarySearch[T]) Depth() int {
 	return calculateDepth[T](t.Root, t._NIL, 0)
 }
 
-// Max returns the Max value of the tree
 func (t *BinarySearch[T]) Max() (T, bool) {
 	ret := maximum[T](t.Root, t._NIL)
 	if ret == t._NIL {
@@ -135,7 +111,6 @@ func (t *BinarySearch[T]) Max() (T, bool) {
 	return ret.Key(), true
 }
 
-// Min returns the Min value of the tree
 func (t *BinarySearch[T]) Min() (T, bool) {
 	ret := minimum[T](t.Root, t._NIL)
 	if ret == t._NIL {
@@ -145,9 +120,7 @@ func (t *BinarySearch[T]) Min() (T, bool) {
 	return ret.Key(), true
 }
 
-// Predecessor returns the Predecessor of the node of Key
-// if there is no predecessor, return default value of type T and false
-// otherwise return the Key of predecessor and true
+// Predecessor 用来查找二叉搜索树中某个节点的前驱节点（predecessor）的值
 func (t *BinarySearch[T]) Predecessor(key T) (T, bool) {
 	node, ok := searchTreeHelper[T](t.Root, t._NIL, key)
 	if !ok {
@@ -157,9 +130,6 @@ func (t *BinarySearch[T]) Predecessor(key T) (T, bool) {
 	return predecessorHelper[T](node, t._NIL)
 }
 
-// Successor returns the Successor of the node of Key
-// if there is no successor, return default value of type T and false
-// otherwise return the Key of successor and true
 func (t *BinarySearch[T]) Successor(key T) (T, bool) {
 	node, ok := searchTreeHelper[T](t.Root, t._NIL, key)
 	if !ok {
@@ -169,8 +139,11 @@ func (t *BinarySearch[T]) Successor(key T) (T, bool) {
 	return successorHelper[T](node, t._NIL)
 }
 
+// pushHelper 是插入操作的辅助函数
 func (t *BinarySearch[T]) pushHelper(x *BSNode[T], val T) {
-	y := t._NIL
+	y := t._NIL // 用于跟踪父节点
+
+	// 查找插入位置
 	for x != t._NIL {
 		y = x
 		switch {
@@ -178,11 +151,12 @@ func (t *BinarySearch[T]) pushHelper(x *BSNode[T], val T) {
 			x = x.left
 		case val > x.Key():
 			x = x.right
-		default:
+		default: // 如果值已存在，直接返回
 			return
 		}
 	}
 
+	// 创建新节点
 	z := &BSNode[T]{
 		key:    val,
 		left:   t._NIL,
@@ -190,22 +164,25 @@ func (t *BinarySearch[T]) pushHelper(x *BSNode[T], val T) {
 		parent: y,
 	}
 
+	// 将新节点连接到树中
 	if y == t._NIL {
-		t.Root = z
+		t.Root = z // 树为空时，设置根节点
 	} else if val < y.key {
-		y.left = z
+		y.left = z // 作为左子节点
 	} else {
-		y.right = z
+		y.right = z // 作为右子节点
 	}
 }
 
+// deleteHelper 是删除操作的辅助函数
 func (t *BinarySearch[T]) deleteHelper(z *BSNode[T]) {
 	switch {
-	case z.left == t._NIL:
+	case z.left == t._NIL: // 没有左子树
 		t.transplant(z, z.right)
-	case z.right == t._NIL:
+	case z.right == t._NIL: // 没有右子树
 		t.transplant(z, z.left)
-	default:
+	default: // 有两个子节点
+		// 找到右子树中的最小节点作为后继
 		y := minimum[T](z.right, t._NIL).(*BSNode[T])
 		if y.parent != z {
 			t.transplant(y, y.right)
@@ -213,22 +190,25 @@ func (t *BinarySearch[T]) deleteHelper(z *BSNode[T]) {
 			y.right.parent = y
 		}
 
+		// 用后继节点替换被删除的节点
 		t.transplant(z, y)
 		y.left = z.left
 		y.left.parent = y
 	}
 }
 
+// transplant 用于在删除操作中替换子树
 func (t *BinarySearch[T]) transplant(u, v *BSNode[T]) {
 	switch {
-	case u.parent == t._NIL:
+	case u.parent == t._NIL: // u是根节点
 		t.Root = v
-	case u == u.parent.left:
+	case u == u.parent.left: // u是左子节点
 		u.parent.left = v
-	default:
+	default: // u是右子节点
 		u.parent.right = v
 	}
 
+	// 更新父节点引用
 	if v != t._NIL {
 		v.parent = u.parent
 	}
--- structure/06_tree/tree.go
+++ structure/06_tree/tree.go
@@ -1,17 +1,10 @@
-// Binary-Search tree is the tree, with the key of each internal node
-// being greater than keys in the respective node's left subtree
-// and less than the ones in its right subtree.
-
-// For more details check out those links below here:
-// Wikipedia article: https://en.wikipedia.org/wiki/Binary_search_tree
-// authors [guuzaa](https://github.com/guuzaa)
 package tree
 
 import (
 	"learn-go/structure/constraints"
-	// "github.com/TheAlgorithms/Go/math/max"
 )
 
+// Node 定义了树节点的通用接口，支持泛型类型T（必须满足Ordered约束）
 type Node[T constraints.Ordered] interface {
 	Key() T
 	Parent() Node[T]
@@ -19,8 +12,7 @@ type Node[T constraints.Ordered] interface {
 	Right() Node[T]
 }
 
-// The following is a collection of helper functions for BinarySearch, AVL and RB.
-
+// accessNodeByLayerHelper 实现树的层序遍历，返回每层节点值的二维切片
 func accessNodeByLayerHelper[T constraints.Ordered](root, nilNode Node[T]) [][]T {
 	if root == nilNode {
 		return [][]T{}
@@ -49,6 +41,7 @@ func accessNodeByLayerHelper[T constraints.Ordered](root, nilNode Node[T]) [][]T
 	return res
 }
 
+// searchTreeHelper 在二叉搜索树中查找指定值，返回找到的节点和是否存在的标志
 func searchTreeHelper[T constraints.Ordered](node, nilNode Node[T], key T) (Node[T], bool) {
 	if node == nilNode {
 		return node, false
@@ -63,25 +56,31 @@ func searchTreeHelper[T constraints.Ordered](node, nilNode Node[T], key T) (Node
 	return searchTreeHelper(node.Right(), nilNode, key)
 }
 
+// inOrderHelper 实现树的中序遍历（非递归方式）
+// 使用栈来模拟递归过程，按照 左-根-右 的顺序访问节点 从小到大返回节点
 func inOrderHelper[T constraints.Ordered](node, nilNode Node[T]) []T {
 	var stack []Node[T]
 	var ret []T
 
 	for node != nilNode || len(stack) > 0 {
+		// 将所有左子节点入栈
 		for node != nilNode {
 			stack = append(stack, node)
 			node = node.Left()
 		}
-
+		// 处理栈顶节点
 		node = stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
 		ret = append(ret, node.Key())
+		// 处理右子节点
 		node = node.Right()
 	}
 
 	return ret
 }
 
+// preOrderRecursive 实现树的前序遍历（递归方式）
+// 按照 根-左-右 的顺序访问节点
 func preOrderRecursive[T constraints.Ordered](n, nilNode Node[T], traversal *[]T) {
 	if n == nilNode {
 		return
@@ -93,6 +92,8 @@ func preOrderRecursive[T constraints.Ordered](n, nilNode Node[T], traversal *[]T
 
 }
 
+// postOrderRecursive 实现树的后序遍历（递归方式）
+// 按照 左-右-根 的顺序访问节点
 func postOrderRecursive[T constraints.Ordered](n, nilNode Node[T], traversal *[]T) {
 	if n == nilNode {
 		return
@@ -103,6 +104,8 @@ func postOrderRecursive[T constraints.Ordered](n, nilNode Node[T], traversal *[]
 	*traversal = append(*traversal, n.Key())
 }
 
+// calculateDepth 计算树的深度
+// 递归计算左右子树的最大深度，返回较大值加1
 func calculateDepth[T constraints.Ordered](n, nilNode Node[T], depth int) int {
 	if n == nilNode {
 		return depth
@@ -111,6 +114,8 @@ func calculateDepth[T constraints.Ordered](n, nilNode Node[T], depth int) int {
 	return maxInt(calculateDepth(n.Left(), nilNode, depth+1), calculateDepth(n.Right(), nilNode, depth+1))
 }
 
+// minimum 查找树中的最小值节点
+// 在二叉搜索树中，最左边的叶子节点即为最小值
 func minimum[T constraints.Ordered](node, nilNode Node[T]) Node[T] {
 	if node == nilNode {
 		return node
@@ -122,6 +127,8 @@ func minimum[T constraints.Ordered](node, nilNode Node[T]) Node[T] {
 	return node
 }
 
+// maximum 查找树中的最大值节点
+// 在二叉搜索树中，最右边的叶子节点即为最大值
 func maximum[T constraints.Ordered](node, nilNode Node[T]) Node[T] {
 	if node == nilNode {
 		return node
@@ -133,6 +140,8 @@ func maximum[T constraints.Ordered](node, nilNode Node[T]) Node[T] {
 	return node
 }
 
+// levelOrderHelper 实现树的层序遍历
+// 使用队列来按层访问节点
 func levelOrderHelper[T constraints.Ordered](root, nilNode Node[T], traversal *[]T) {
 	var q []Node[T] // queue
 	var tmp Node[T]
@@ -152,11 +161,14 @@ func levelOrderHelper[T constraints.Ordered](root, nilNode Node[T], traversal *[
 	}
 }
 
+// predecessorHelper 查找指定节点的前驱节点
+// 前驱节点是中序遍历中当前节点的前一个节点
 func predecessorHelper[T constraints.Ordered](node, nilNode Node[T]) (T, bool) {
+	// 如果有左子树，则前驱是左子树中的最大值
 	if node.Left() != nilNode {
 		return maximum(node.Left(), nilNode).Key(), true
 	}
-
+	// 如果没有左子树，则向上查找第一个将当前节点作为右子树的祖先节点
 	p := node.Parent()
 	for p != nilNode && node == p.Left() {
 		node = p
@@ -170,11 +182,15 @@ func predecessorHelper[T constraints.Ordered](node, nilNode Node[T]) (T, bool) {
 	return p.Key(), true
 }
 
+// successorHelper 查找指定节点的后继节点
+// 后继节点是中序遍历中当前节点的后一个节点
 func successorHelper[T constraints.Ordered](node, nilNode Node[T]) (T, bool) {
+	// 如果有右子树，则后继是右子树中的最小值
 	if node.Right() != nilNode {
 		return minimum(node.Right(), nilNode).Key(), true
 	}
 
+	// 如果没有右子树，则向上查找第一个将当前节点作为左子树的祖先节点
 	p := node.Parent()
 	for p != nilNode && node == p.Right() {
 		node = p
